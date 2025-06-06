ff96c53e5a6fbbeee2aecb92b2e58c94ae47fa99
esehiyoko <ese.hiyoko@gmail.com>
2025-06-07 00:43:36 +0900

    Refactor logging in CustomLogger to improve loss tracking and update log structure

--- custom_metrics/custom_logger.py
+++ custom_metrics/custom_logger.py
@@ -45,19 +45,21 @@ class CustomLogger:
         self.loss_sum += loss
         self.loss_count += 1
 
-        if global_step % 10 == 0:
+        if global_step % 5 == 0:
             raw_avg_loss = self.loss_sum / self.loss_count
             effective_step = global_step * self.accumulation
 
             self.wandb.log({
-                "current_loss": loss,
-                "raw_average_loss": raw_avg_loss,
-                "global_step": global_step,
-                "effective_step": effective_step
+                "loss/current_loss": loss,
+                "loss/raw_average_loss": raw_avg_loss,
             }, step=effective_step)
 
-            self.loss_sum = 0.0
-            self.loss_count = 0
+        self.wandb.log({
+            "loss/current": loss,
+        }, step=effective_step)
+
+        self.loss_sum = 0.0
+        self.loss_count = 0
 
     def log_named(self, name, value, global_step):
 
